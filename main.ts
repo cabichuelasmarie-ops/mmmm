2f758d4ca41d21f59c8860c8e301d187e505634a
cabichuelasmarie-ops <cabichuelas.marie@gmail.com>
2025-11-21 15:54:41 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    myCorg.bark()
+})
+let myCorg: Corgio = null
+myCorg = corgio.create(SpriteKind.Player)
+myCorg.updateSprite()
+myCorg.horizontalMovement()
+myCorg.verticalMovement()
+myCorg.maxJump = 1
